444bde1b87bd718ed035b3db49bc445b19864bda
Nicholas Adams <142557098+Connora711@users.noreply.github.com>
2024-01-26 19:56:24 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Here's YouTube! The biggest video platform on the web.
+            Here&apos;s YouTube! The biggest video platform on the web.
           </p>
         </a>
 
